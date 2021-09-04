51173423275bde0a2cc8a77ed7c019e031d84a0c
Aman Maurya <amanmaury36@gmail.com>
2021-09-04 14:19:35 +0530

    Added backend functionality for Home Page, Topics Page and Individual Topic View Page

---
+++ learning_logs/urls.py
@@ -0,0 +1,15 @@
+''' Defines URL patterns for learning_logs '''
+
+from django.conf.urls import url, include
+from . import views
+
+urlpatterns = [
+    # Home Page
+    url(r'^$', views.index, name='index'),
+
+    #Show all topics
+    url(r'^topics/$', views.topics, name = 'topics'),
+
+    #Detailed page for a single topic
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name = 'topic'),
+]
