4804a982f93b8b8b89712bd722fb2e154d96ec87
Aman Maurya <amanmaury36@gmail.com>
2021-09-07 22:34:11 +0530

    Added a new topic page for users to create new topics.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,9 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
+
 from .models import Topic
+from .forms import TopicForm
 
 def index(request):
     ''' Learning Log Home Page '''
@@ -17,3 +21,17 @@ def topic(request, topic_id):
     entries= topic.entry_set.order_by('-date_added')
     context = { 'topic' : topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Add a new Topic. """
+    if request.method != 'POST':
+        #No data submitted; Create a blank form.
+        form = TopicForm()
+    else:
+        #POST data submitted; Process data.
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+    context = {'form' : form}
+    return render(request, 'learning_logs/new_topic.html', context)
